f83dfef98dec7254705c16f1d26fa6927002ab01
keinich <rafael.weschle@gmail.com>
2024-04-15 21:59:16 +0200

    Updated fusefx-repositorycontract version

--- src/DataSourceService.ts
+++ src/DataSourceService.ts
@@ -16,7 +16,6 @@ export function getParentFilter(
   }
   const childToParentRelation: RelationSchema = childToParentRelations[0];
   const result: LogicalExpression = new LogicalExpression();
-  result.operator = '';
 
   const pk: IndexSchema | null = pks.length > 0 ? pks[0] : null;
   if (!pk || pk.memberFieldNames.length !== 1) {
@@ -31,11 +30,10 @@ export function getParentFilter(
 
   const parentIdFieldType: string = parentField ? parentField.type : 'int';
   const parentIdName: string = parentIdFieldName || 'id';
-  result.atomArguments.push({
-    relation: '=',
-    propertyName: childToParentRelation.foreignKeyIndexName,
+  result.predicates.push({
+    operator: '=',
+    fieldName: childToParentRelation.foreignKeyIndexName,
     value: parent[parentIdName],
-    propertyType: parentIdFieldType,
   });
   return result;
 }
